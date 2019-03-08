c95f841e9013502d8cd2ff6044fe43b2cfc93bb1
zhangliming <zhangliming@dianwoba.com>
2019-03-08 11:18:50 +0800

    测试图片

--- visualize.py
+++ visualize.py
@@ -89,8 +89,8 @@ def main(args=None):
 				cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
 				print(label_name)
 
-			cv2.imshow('img', img)
-			cv2.waitKey(0)
+			cv2.imwrite('img', img)
+			#cv2.waitKey(0)
 
 
 
